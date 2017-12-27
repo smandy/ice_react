06c5cddefb7bcbbf3e1efdcbc523531daeb4dbb4
Andy Smith <andyrsmith@gmail.com>
2017-12-27 10:11:12 +0000

    Incremental.

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -6,43 +6,55 @@ var communicator = Ice.initialize([], id);
 console.log("Communicator is " + communicator);
 var proxies = ["foo@SimpleApp", "foo@SimpleJavaApp", "foo@SimpleCppApp"];
 
-var retVal;
-
-proxies.forEach( function (x) {
-    var proxy = communicator.stringToProxy(x).ice_timeout(5000);
-    var x2 = argo.FooPrx.checkedCast(proxy).then(
-    function(prx) {
-        //console.log("Called with x " + prx );
-        prx.doit().then(
-            function (ret) {
-                retVal = ret;
-                console.log("Ret is " + ret + " from " + x);
-            },
-            function (ex) {
-                console.log(ex);
-            }
-        );
-    },
-    function(ex) {
-        console.log("Called with exception " + ex);
-    } );
-});
-
 class Foo extends React.Component {
     constructor() {
         super();
     };
 
+    state = {
+        responses : []
+    };
+
     componentDidMount() {
-        this.setState([]);
+        //this.setState([]);
+        const outer = this;
+        proxies.forEach( function (x) {
+            var proxy = communicator.stringToProxy(x).ice_timeout(5000);
+            var x2 = argo.FooPrx.checkedCast(proxy).then(
+                function(prx) {
+                    //console.log("Called with x " + prx );
+                    prx.doit().then(
+                        function (ret) {
+                            //console.log("Ping");
+                            outer.setState( { responses :
+                                              outer.state.responses.concat( {
+                                                  response: ret,
+                                                  uid: uuid.v4()
+                                              }) });
+                            //console.log("Ret is " + ret + " from " + x);
+                        },
+                        function (ex) {
+                            console.log(ex);
+                        }
+                    );
+                },
+                function(ex) {
+                    console.log("Called with exception " + ex);
+                } );
+        });
     };
 
     render() {
         const str = communicator.toString();
-
+        const responses = this.state.responses.map( x => {
+            //const uid = uuid.v4();
+            return (  <h2 key={x.uid}>response is {x.response}</h2> );
+        } );
         return (
             <h1>Hello from react</h1>,
-            <h2>communicator is {str}</h2>
+            <div>
+              {responses}
+            </div>
         );
     };
 };
