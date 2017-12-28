b536e2d10ff3c3e965f9374bbc84f52ac7309d72
Andy Smith <andyrsmith@gmail.com>
2017-12-28 22:25:57 +0000

    Dodgy attempt to get ice + react integrated ( not using react-create-app).

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -54,4 +54,104 @@ class Foo extends React.Component {
     };
 };
 
+class MyListener extends argo.TickListener {
+    state = {
+        listeners: []
+    };
+
+    addListener(listener) {
+        this.state.listeners.concat(listener);
+    };
+    
+    onImage( img, current) {
+        this.state.listeners.forEach( x => x.onImage(img, current));
+        // if (img.lenth>0) {
+        //     img[0].forEach( function( tick, idx, _) {
+        //         data[idx] = tick;
+        //         grid.invalidateRow(idx);
+        //     });
+        // };
+    };
+    
+    onTick(ticks, current) {
+        this.state.listeners.forEach( x => x.onTick(ticks,current));
+        //$("#symbol").html("<pre>" + ticks[0]['symbol'] + "</pre>");
+        //$("#bid").html("<pre>" + ticks[0]['bidPx'] + "</pre>");
+        //$("#ask").html("<pre>" + ticks[0]['askPx'] + "</pre>");
+        // for (i = 0;i<ticks.length;i++) {
+        //     data[i] = ticks[i];
+        //     grid.invalidateRow(i);
+        // }
+        // grid.render();
+    };
+}
+
+class DataGrid extends React.Component {
+    state = {
+        data: []
+    };
+
+    componentDidMount() {
+        this.props.source.addListener(this);
+    };
+    
+    onTick(ticks, current) {
+        let newData = this.state.data.slice();
+        for (i = 0;i<ticks.length;i++) {
+            newData[i] = ticks[i];
+            //grid.invalidateRow(i);
+        }
+        this.setState( { data: newData });
+    };
+
+    onImage(img, current) {
+        let newData = [];
+        if (img.length>0) {
+            img[0].forEach( function(tick, idx, _) {
+                newData[idx] = tick;
+            });
+            this.setState( { data: newData});
+        }
+    };
+
+    render() {
+        const rows = this.state.data.map( x => {
+            return (<tr><td>{x.symbol}</td><td>{x.bidPx}</td><td>{x.askPx}</td></tr>);
+        });
+        
+        return (
+            <table>{rows}</table>
+        );
+    };
+}
+
+var proxy = communicator.stringToProxy("plant:ws -h ritz -p 10666 -t 2000").ice_twoway();
+
+var tl = new MyListener();
+
+argo.TickerPlantPrx.checkedCast(proxy).then(
+    function(prx) {
+        setInterval( function() {
+            prx.sayHello().then( function (s) {
+                //console.log("hello returned " + s );
+                $("#greeting").html( "<H1>" + "Got " + s + "</H1>" );
+            } );
+        } , 2000);
+        
+        return communicator.createObjectAdapter("").then(
+            function(adapter) {
+                var r = adapter.addWithUUID();
+                proxy.ice_getCachedConnection().setAdapter(adapter);
+                var x3 = prx.subscribeWithIdent(r.ice_getIdentity());
+            }
+        );
+    },
+    function(ex) {
+        console.log("Called with exception " + ex);
+    } );
+
 ReactDOM.render( <Foo/>,  document.getElementById("content"));
+    
+ReactDOM.render( <DataGrid source={tl}/>,  document.getElementById("grid"));
+
+
