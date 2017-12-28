91965163eee2e1df09878e69574a2c9ccbfcbe00
Andy Smith <andyrsmith@gmail.com>
2017-12-28 21:40:38 +0000

    Incremental.

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -16,22 +16,18 @@ class Foo extends React.Component {
     };
 
     componentDidMount() {
-        //this.setState([]);
         const outer = this;
         proxies.forEach( function (x) {
             var proxy = communicator.stringToProxy(x).ice_timeout(5000);
             var x2 = argo.FooPrx.checkedCast(proxy).then(
                 function(prx) {
-                    //console.log("Called with x " + prx );
                     prx.doit().then(
                         function (ret) {
-                            //console.log("Ping");
                             outer.setState( { responses :
                                               outer.state.responses.concat( {
                                                   response: ret,
                                                   uid: uuid.v4()
                                               }) });
-                            //console.log("Ret is " + ret + " from " + x);
                         },
                         function (ex) {
                             console.log(ex);
@@ -47,8 +43,7 @@ class Foo extends React.Component {
     render() {
         const str = communicator.toString();
         const responses = this.state.responses.map( x => {
-            //const uid = uuid.v4();
-            return (  <h2 key={x.uid}>response is {x.response}</h2> );
+            return (  <p key={x.uid}>response is {x.response}</p> );
         } );
         return (
             <h1>Hello from react</h1>,
